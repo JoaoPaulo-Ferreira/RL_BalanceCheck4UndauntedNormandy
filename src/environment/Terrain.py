2018703edc449d4c2e786a6e0220b4d7b1e2e84d
joaopaulo-ferreira <joaopaulof.soares@gmail.com>
2026-02-18 11:21:29 -0400

    defined all tiles and improving loading strategy

--- src/environment/Terrain.py
+++ src/environment/Terrain.py
@@ -29,9 +29,16 @@ class Map:
         self.scenario_name = ""
         self.TileMap = {}
         self.adjacency = 0
+        # self.tiles_file = os.getenv('tiles_filepath')
+        self.all_tiles = []
+        self._load_all_tiles()
     
-    def get_distance(self, tile_origin, tile_destiny):
+    def get_shortests_distance(self, tile_origin, tile_destiny):
         return nx.shortest_path_length(self.adjacency, source=tile_origin, target=tile_destiny)
+
+    def _load_all_tiles(self):
+        with open(os.getenv('tiles_filepath'), 'r') as f:
+            self.all_tiles = yaml.load(f, Loader=yaml.SafeLoader)
 
     def load_map(self, conf_path):
         with open(conf_path, 'r') as f:
@@ -40,15 +47,24 @@ class Map:
         map_name = data.get('Name', 'Default_name')
         tile_list = data.get('Tiles', [])
         adj_list = data.get('Adjacency', [])
-        if tile_list == [] or adj_list == []:
+        markers = data.get('Markers', [])
+        if tile_list == [] or adj_list == [] or markers == []:
             raise RuntimeError("Impossible to load the Map")
-
+        print(self.all_tiles)
+        print(tile_list)
+        print(adj_list)
+        print(markers)
+        print("all_tiles:\n", self.all_tiles)
         print('Loading Map -> Tiles...')
-        for tile_name, tile_att in tile_list.items():
-            outter_shield = tile_att.get('outter_s', -1)
-            inner_shield = tile_att.get('inner_s', outter_shield)
+        for tile_name in tile_list:
+            # print(f"tile_name: {tile_name}")
+            # print(f"self.all_tiles.get('tile_name'): {self.all_tiles.get(tile_name)}")
+            tile_info = self.all_tiles.get(tile_name)
+            # print('tile_info: ', tile_info)
+            outter_shield = tile_info.get('outter_shield')
+            inner_shield = tile_info.get('inner_shield')
             self.TileMap[tile_name] = Tile(name=tile_name, inner_s = inner_shield, outter_s = outter_shield)
-            print(tile_list)
+            # self.TileMap[tile] = Tile(name=tile_list, inner_s = inner_shield, outter_s = outter_shield)
 
         print('Tiles loaded.\n')
         print('Loading Map -> Adjacency...')
@@ -79,7 +95,7 @@ if __name__ == '__main__':
     from dotenv import load_dotenv
     load_dotenv()
     map = Map()
-    map.load_map(os.getenv('file_path'))
+    map.load_map(os.getenv('map1_filepath'))
     map.to_string()
-    print(map.get_distance('A1', 'B2'))
-    print(map.get_distance('A1', 'C1'))
+    print(map.get_shortests_distance('11B', '2B'))
+    print(map.get_shortests_distance('7A', '6A'))
